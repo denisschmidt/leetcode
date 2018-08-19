5ab1cf573a63b70f994543ad66140e04792197a7
Denis Samsonov <denis.samsonov@csssr.com>
2018-08-19 19:43:30 +0300

    first commit

---
+++ string/3. Longest Substring Without Repeating Characters/index.js
@@ -0,0 +1,31 @@
+/*
+
+Given a string, find the length of the longest substring without repeating characters.
+
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", which the length is 3.
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = s => {
+  
+
+  
+
+}
