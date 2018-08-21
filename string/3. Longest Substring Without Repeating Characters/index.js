f327d17d91898435fef5bd2e2259b31c9bc8db8b
Denis Samsonov <denis.samsonov@csssr.com>
2018-08-21 04:45:17 +0300

    string

--- string/3. Longest Substring Without Repeating Characters/index.js
+++ string/3. Longest Substring Without Repeating Characters/index.js
@@ -17,10 +17,6 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3. 
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-
-Примерный алгоритм работы
-  1)  
-
 */
 
 /**
@@ -28,8 +24,19 @@ Explanation: The answer is "wke", with the length of 3.
  * @return {number}
  */
 const lengthOfLongestSubstring = s => {
-  
-
-  
-
+  let countArr = []
+  let currentStr = ''
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i; j < s.length; j++) {
+      if (currentStr.indexOf(s[j]) === -1) {
+        currentStr = currentStr + s[j]
+      } else {
+        countArr.push(currentStr.length)
+        currentStr = ''
+        break
+      }  
+    }
+  }
+  countArr.push(currentStr.length)
+  return countArr.sort((a, b) => b - a)[0]  
 }
