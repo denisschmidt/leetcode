a8e5de9d8dbfd0bd152a7a22ce881e22c5e4c093
denisschmidt <parasolgroup25@gmail.com>
2018-10-07 09:55:17 +0300

    tree

---
+++ tree/199. Binary Tree Right Side View/index.js
@@ -0,0 +1,109 @@
+/*
+Given a binary tree, imagine yourself standing on the right side of it,
+return the values of the nodes you can see ordered from top to bottom.
+
+Example:
+
+Input: [1,2,3,null,5,null,4]
+Output: [1, 3, 4]
+Explanation:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+const { makeTreeNodes, TreeNode } = require('../../utils')
+
+/**
+ * Approach #1 Depth-First Search
+ *
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+let tree = new TreeNode()
+
+const rightSideView = function(root) {
+  let nodeStack = [], depthStack = []
+  let maxDepth = -1
+  let rightmostValueAtDepth = new Map()
+  nodeStack.push(root)
+  depthStack.push(0)
+
+  while (nodeStack.length) {
+    let node = nodeStack.pop()
+    let depth = depthStack.pop()
+
+    if (node !== null) {
+      maxDepth = Math.max(maxDepth, depth)
+      if (!rightmostValueAtDepth.has(depth)) {
+        rightmostValueAtDepth.set(depth, node.val)
+      }
+      nodeStack.push(node.left)
+      nodeStack.push(node.right)
+      depthStack.push(depth + 1)
+      depthStack.push(depth + 1)
+    }
+  }
+  const arr = []
+  for(let i = 0; i <= maxDepth; i++) {
+    arr.push(rightmostValueAtDepth.get(i))
+  }
+  return arr
+}
+// const res = rightSideView(makeTreeNodes([1, 2, 3, null, 5, null, 4]))
+// console.log('====', res)
+
+/*
+Approach #2 Breadth-First Search
+ */
+
+/**
+ * Approach #1 Depth-First Search
+ *
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+const rightSideView2 = function(root) {
+  let nodeQueue = [], depthQueue = []
+  let maxDepth = -1
+  let rightmostValueAtDepth = new Map()
+  nodeQueue.push(root)
+  depthQueue.push(0)
+
+  while (nodeQueue.length) {
+    let node = nodeQueue.shift()
+    let depth = depthQueue.shift()
+
+    if (node !== null) {
+      console.log('===', node.val)
+      maxDepth = Math.max(maxDepth, depth)
+      rightmostValueAtDepth.set(depth, node.val)
+
+      nodeQueue.push(node.left)
+      nodeQueue.push(node.right)
+      depthQueue.push(depth + 1)
+      depthQueue.push(depth + 1)
+    }
+  }
+  const res = []
+  for(let i=0;i<=maxDepth;i++) {
+    res.push(rightmostValueAtDepth.get(i))
+  }
+  return res
+}
+const res = rightSideView2(makeTreeNodes([1, 2, 3, null, 5, null, 4]))
--- utils.js
+++ utils.js
@@ -1,19 +1,18 @@
-'use strict';
+'use strict'
 
-const _ = require('lodash');
+const _ = require('lodash')
 
 class LinkNode {
   constructor(val) {
-    this.val = val;
-    this.next = null;
+    this.val = val
+    this.next = null
   }
 }
 
 class TreeNode {
   constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
+    this.val = val
+    this.left = this.right = null
   }
 }
 
@@ -73,66 +72,66 @@ class DSU {
 
 
 function makeTestLinkNodes(tests, name = 'root') {
-  makeTestNodes(makeLinkNodes, tests, name);
+  makeTestNodes(makeLinkNodes, tests, name)
 }
 
 function makeTestTreeNodes(tests, name = 'root') {
-  makeTestNodes(makeTreeNodes, tests, name);
+  makeTestNodes(makeTreeNodes, tests, name)
 }
 
 function makeTestNodes(iterator, tests, name = 'root') {
   _.forEach(tests, test => {
-    const array = test[name];
-    const root = iterator(array);
-    test[name] = root;
-    test[`_${name}`] = array;
-  });
+    const array = test[name]
+    const root = iterator(array)
+    test[name] = root
+    test[`_${name}`] = array
+  })
 }
 
 function makeLinkNodes(array) {
   return _.chain(array)
     .map(n => new LinkNode(n))
     .reduce((result, node) => {
-      let target = result;
+      let target = result
       while (target.next) {
-        target = target.next;
+        target = target.next
       }
-      target.next = node;
-      return result;
+      target.next = node
+      return result
     })
-    .value();
+    .value()
 }
 
 function makeTreeNodes(array) {
-  const l = array.length;
+  const l = array.length
   if (!l) {
-    return null;
+    return null
   }
-  let i = 0;
-  const root = new TreeNode(array[i++]);
-  makeTree([root]);
-  return root;
+  let i = 0
+  const root = new TreeNode(array[i++])
+  makeTree([root])
+  return root
 
   function makeTree(parents) {
     if (i >= l) {
-      return;
+      return
     }
-    const children = [];
-    _.forEach(parents, parent => {
-      const left = array[i++];
-      const right = array[i++];
+    const children = []
+    parents.forEach(parent => {
+      const left = array[i++]
+      const right = array[i++]
       if (left !== null && left !== undefined) {
-        const node = new TreeNode(left);
-        parent.left = node;
-        children.push(node);
+        const node = new TreeNode(left)
+        parent.left = node
+        children.push(node)
       }
       if (right !== null && right !== undefined) {
-        const node = new TreeNode(right);
-        parent.right = node;
-        children.push(node);
+        const node = new TreeNode(right)
+        parent.right = node
+        children.push(node)
       }
-    });
-    makeTree(children);
+    })
+    makeTree(children)
   }
 }
 
@@ -144,4 +143,4 @@ module.exports = {
   makeTestLinkNodes,
   makeTestTreeNodes,
   DSU
-};
+}
