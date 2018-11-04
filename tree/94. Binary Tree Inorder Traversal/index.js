86ff9122126a8fe22a13ef9e4aacc5bc38842d9a
denisschmidt <parasolgroup25@gmail.com>
2018-11-04 16:12:57 +0300

    tree inorder

---
+++ tree/94. Binary Tree Inorder Traversal/index.js
@@ -0,0 +1,61 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+Iterating method using Stack:
+
+Complexity Analysis
+
+Time complexity : O(n)O(n).
+
+Space complexity : O(n)O(n).
+
+
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+const { makeTreeNodes, TreeNode } = require('../../utils');
+
+const inorderTraversal = function(root) {
+  let nodeStack = [], node = root;
+  const resArr = [];
+
+  while(nodeStack.length > 0 || node !== null) {
+    while(node !== null) {
+      nodeStack.push(node);
+      node = node.left;
+    }
+    node = nodeStack.pop();
+    resArr.push(node.val);
+    node = node.right;
+  }
+  return resArr;
+};
+
+const res = inorderTraversal(makeTreeNodes([1, null, 2, 3]));
+console.log('===', res);
+
+
