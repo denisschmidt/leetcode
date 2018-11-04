1b3730513ce9845232ddc20c358e6a582de429df
denisschmidt <parasolgroup25@gmail.com>
2018-11-04 16:40:41 +0300

    string

---
+++ string/125. Valid Palindrome/index.js
@@ -0,0 +1,25 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Example 2:
+Input: "race a car"
+Output: false
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  const str = s.replace(/[^A-Za-z0-9]+/g, '');
+  return str.split('').reverse().join('').toLowerCase() === str.toLowerCase();
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
