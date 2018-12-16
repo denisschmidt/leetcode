6750e31d2721ca79f0f5ddfc290b4a8a13a844ce
denisschmidt <parasolgroup25@gmail.com>
2018-12-16 14:58:52 +0300

    Tree, DFS

--- array/00 Generate Gray Codes/index.js
+++ array/00 Generate Gray Codes/index.js
@@ -24,13 +24,13 @@
 
 Кол-во таких последовательностей 2 ^ n
 
-
  */
+
+
 
 /*
   n - число последовательностей
  */
-
 const generateSequence = (num, n) => {
   let arr = [];
   for (let i = 0; i < n; i++) {
@@ -39,14 +39,22 @@ const generateSequence = (num, n) => {
   return arr;
 };
 
-const generateCodes = (n, k) => {
+/*
+  Сгенерировать все k-мерные коды Грея длиной n.
+
+  n - длинна кода для 4 = [0, 0, 0, 1]
+  k - мерные коды для 2 коды могут содержать только 1 и 0
+ */
+
+const generateCodes = (n = 3, k = 3) => {
   let index;
-  let arr = generateSequence(0, 3);
-  let direction = generateSequence(1, 3);
-  if (!n || !k) return null;
+  let arr = generateSequence(0, n);
+  let direction = generateSequence(1, n);
+  let res = [];
   while (true) {
     index = n - 1;
     while (index >= 0) {
+      // условие на нахождение столбца, который можно двигать
       if ((arr[index] === 0 && direction[index] === 0) || (arr[index] === k - 1 && direction[index] === 1)) {
         direction[index] = (direction[index] + 1) % 2;
       } else {
@@ -54,16 +62,16 @@ const generateCodes = (n, k) => {
       }
       index -= 1
     }
+    // если не нашли такого столбца, то алгоритм закончил работу
     if (index < 0) {
       break;
     }
+    // либо двигаем на 1 вперед, либо на 1 назад
     arr[index] += direction[index]*2 - 1;
-
+    res.push(arr);
     console.log('----', arr)
   }
+  return res;
 };
 
-const res = generateCodes(3, 3);
-console.log('---', res);
-
-
+const res = generateCodes(2, 2);
---
+++ tree/938. Range Sum of BST/index.js
@@ -0,0 +1,57 @@
+/*
+
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+
+
+Example 1:
+
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+const { makeTreeNodes } = require('../../utils')
+
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+const rangeSumBST = function(root, L, R) {
+  let nodeStack = [], valuesArr = [];
+  nodeStack.push(root);
+
+  while (nodeStack.length) {
+    let node = nodeStack.pop();
+
+    if (node !== null) {
+      nodeStack.push(node.left);
+      nodeStack.push(node.right);
+      if (node.val >= L && node.val <= R) {
+        valuesArr.push(node.val)
+      }
+    }
+  }
+  // console.log('---', valuesArr);
+  return valuesArr.reduce((acc, item) => acc + item, 0);
+};
+
+const res = rangeSumBST(makeTreeNodes([10,5,15,3,7,13,18,1,null,6]), 6, 10);
+
+console.log('---', res);
