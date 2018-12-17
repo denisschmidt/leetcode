79ab6a7e93c718a544bf37756e83dd380d8cd070
denisschmidt <parasolgroup25@gmail.com>
2018-12-17 08:27:45 +0300

    Array

---
+++ array/942. DI String Match/index.js
@@ -0,0 +1,67 @@
+/*
+Given a string S that only contains "I" (increase) or "D" (decrease), let N = S.length.
+
+Return any permutation A of [0, 1, ..., N] such that for all i = 0, ..., N-1:
+
+If S[i] == "I", then A[i] < A[i+1]
+If S[i] == "D", then A[i] > A[i+1]
+
+
+Example 1:
+
+Input: "IDID"
+Output: [0,4,1,3,2]
+Example 2:
+
+Input: "III"
+Output: [0,1,2,3]
+Example 3:
+
+Input: "DDI"
+Output: [3,2,0,1]
+
+
+Note:
+
+1 <= S.length <= 10000
+S only contains characters "I" or "D".
+
+ */
+
+/**
+ * @param {string} str
+ * @return {number[]}
+ */
+const diStringMatch = str => {
+  let res = [], iSum = 0, dSum = str.length;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'I') {
+      if (iSum === 0) {
+        res.push(iSum);
+        iSum += 1;
+      } else  {
+        res.push(iSum);
+        iSum += 1;
+      }
+      if (i === str.length - 1) {
+        res.push(iSum);
+      }
+    } else if (str[i] === 'D') {
+      if (dSum === str.length) {
+        res.push(dSum);
+        dSum -= 1;
+      } else  {
+        res.push(dSum);
+        dSum -= 1;
+      }
+      if (i === str.length - 1) {
+        res.push(dSum);
+      }
+    }
+  }
+  console.log('---', res);
+  return res;
+};
+
+diStringMatch('IDID');
