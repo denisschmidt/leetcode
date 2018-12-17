e4388bf81d8ceec42855fdbcab78eff432ee8d96
denisschmidt <parasolgroup25@gmail.com>
2018-12-17 07:39:19 +0300

    Graph

---
+++ graph/797. All Paths From Source to Target/index.js
@@ -0,0 +1,87 @@
+/*
+Given a directed, acyclic graph of N nodes.
+
+Find all possible paths from node 0 to node N-1, and return them in any order.
+
+The graph is given as follows:
+  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+Example:
+  Input: [[1,2], [3], [3], []]
+  Output: [[0,1,3],[0,2,3]]
+
+Explanation: The graph looks like this:
+  0--->1
+  |    |
+  v    v
+  2--->3
+
+There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+
+Note:
+
+The number of nodes in the graph will be in the range [2, 15].
+You can print different paths in any order, but you should keep the order of nodes inside one path.
+
+ */
+
+/**
+ * DFS Solution
+ *
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+const allPathsSourceTarget = graph => {
+  let res = [], path = [];
+  path.push(0);
+  dfsSearch(0);
+
+  function dfsSearch(node) {
+    if (node === graph.length - 1){
+      res.push(path.slice(0));
+      return;
+    }
+    for(let nextNode of graph[node]) {
+      path.push(nextNode);
+      dfsSearch(nextNode);
+      path.pop();
+    }
+  }
+  console.log('====', res);
+  return res;
+};
+
+allPathsSourceTarget([[1,2], [3], [3], []]);
+
+
+/**
+ * DFS Solution
+ *
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+const allPathsSourceTarget2 = graph => {
+  let results = [];
+  const dfs = (graph, node = 0, step = 0, path = []) => {
+    if (node === graph.length - 1) {
+      path[step] = node;
+      results.push(path.slice(0, step + 1));
+      return;
+    }
+
+    if (!graph[node] || graph[node].length === 0) {
+      return;
+    }
+
+    path[step] = node;
+
+    for (let i = 0; i < graph[node].length; i++) {
+      dfs(graph, graph[node][i], step + 1, path);
+    }
+  };
+  dfs(graph);
+  console.log('====', results);
+  return results;
+};
+
+allPathsSourceTarget2([[1,2], [3], [3], []]);
--- graph/841. Keys and Rooms !!!!!!!!!!/index.js
+++ graph/841. Keys and Rooms !!!!!!!!!!/index.js
@@ -1,13 +1,13 @@
 /*
-There are N rooms and you start in room 0. 
-Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to 
-access the next room. 
+There are N rooms and you start in room 0.
+Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to
+access the next room.
 
-Formally, each room i has a list of keys rooms[i], 
-and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  
+Formally, each room i has a list of keys rooms[i],
+and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.
 A key rooms[i][j] = v opens the room with number v.
 
-Initially, all the rooms start locked (except for room 0). 
+Initially, all the rooms start locked (except for room 0).
 
 You can walk back and forth between rooms freely.
 
@@ -20,7 +20,7 @@ Input: [[1],[2],[3],[]]
 
 
 Output: true
-Explanation:  
+Explanation:
 We start in room 0, and pick up key 1.
 We then go to room 1, and pick up key 2.
 We then go to room 2, and pick up key 3.
@@ -47,18 +47,20 @@ Space Complexity: O(N)O(N) in additional space complexity, to store stack and se
  * @return {boolean}
  */
 var canVisitAllRooms = function(rooms) {
-  let seen = [], stack = [] 
+  let seen = [], stack = []
   seen[0] = true
   stack.push(0)
-  
+
   while(stack.length) {
     let node = stack.pop() // Получаем следующий ключ узла
+
     for(let i=0; i < rooms[node].length; i++){
       if(!seen[rooms[node][i]]) {
         seen[rooms[node][i]] = true // отметка того что мы открыли комнату
         stack.push(rooms[node][i]) // добавим ключ в стек
       }
     }
+
   }
   for(let i=0; i < rooms.length; i++) {
     if(!seen[i]) return false
