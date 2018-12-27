91d01472946007c185d6b4360b20d70b75b830cf
denisschmidt <parasolgroup25@gmail.com>
2018-12-27 19:05:34 +0300

    string intresting solution

--- string/899. Orderly Queue/index.js
+++ string/899. Orderly Queue/index.js
@@ -7,4 +7,26 @@ remove it, and place it at the end of the string.
 Return the lexicographically smallest string we could have after any number of moves.
  */
 
+/**
+ * @param {string} str
+ * @param {number} len
+ * @return {string}
+ */
+const orderlyQueue = function(str, k) {
+  if ( k === 1) {
+    let res = str, cur = str;
+    for (let i = 0; i < str.length; i++) {
+      cur = cur.substring(1) + cur[0];
+      if (res > cur) {
+        res = cur;
+      }
+    }
+    return res;
+  } else {
+    return str.split('').sort().join('');
+  }
+};
 
+const res = orderlyQueue('cba', 1); // "guozmw"
+
+console.log('---', res);
