22dc68a786496541a906c7ea74c1260766c1b54a
denisschmidt <parasolgroup25@gmail.com>
2019-01-07 11:50:05 +0300

    add prettier

--- string/3. Longest Substring Without Repeating Characters/index.js
+++ string/3. Longest Substring Without Repeating Characters/index.js
@@ -4,7 +4,7 @@ Given a string, find the length of the longest substring without repeating chara
 
 
 Input: "abcabcbb"
-Output: 3 
+Output: 3
 Explanation: The answer is "abc", which the length is 3.
 
 Input: "bbbbb"
@@ -14,7 +14,7 @@ Explanation: The answer is "b", with the length of 1.
 
 Input: "pwwkew"
 Output: 3
-Explanation: The answer is "wke", with the length of 3. 
+Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 */
@@ -24,19 +24,22 @@ Explanation: The answer is "wke", with the length of 3.
  * @return {number}
  */
 const lengthOfLongestSubstring = s => {
-  let countArr = []
-  let currentStr = ''
+  let countArr = [];
+  let currentStr = '';
   for (let i = 0; i < s.length; i++) {
     for (let j = i; j < s.length; j++) {
       if (currentStr.indexOf(s[j]) === -1) {
-        currentStr = currentStr + s[j]
+        currentStr = currentStr + s[j];
       } else {
-        countArr.push(currentStr.length)
-        currentStr = ''
-        break
-      }  
+        countArr.push(currentStr.length);
+        currentStr = '';
+        break;
+      }
     }
   }
-  countArr.push(currentStr.length)
-  return countArr.sort((a, b) => b - a)[0]  
-}
+  countArr.push(currentStr.length);
+  return countArr.sort((a, b) => b - a)[0];
+};
+
+const res = lengthOfLongestSubstring('abcabcbb');
+console.log('---', res);
