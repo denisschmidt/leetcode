85fcc3743865f709272b9c265c8063b02424c475
denisschmidt <parasolgroup25@gmail.com>
2019-01-13 18:53:14 +0300

    task => Binary Tree Postorder Traversal(DFS solution)

--- tree/145. Binary Tree Postorder Traversal/index.js
+++ tree/145. Binary Tree Postorder Traversal/index.js
@@ -23,6 +23,19 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
  *     this.left = this.right = null;
  * }
  */
+
+/*
+  Solution using DFS
+
+  Time complexity :
+    we visit each node exactly once, thus the time complexity is O(N),
+    where N is the number of nodes, i.e. the size of tree.
+
+  Space complexity: depending on the tree structure, we could keep up to the entire tree,
+    therefore, the space complexity is O(N).
+
+ */
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
@@ -45,5 +58,26 @@ const postorderTraversal = function(root) {
 };
 
 const res = postorderTraversal(makeTreeNodes([1, null, 2, 3]));
-
 console.log('---', res);
+
+const postorderTraversal2 = function(root) {
+  let nodeQueue = [];
+  let arr = [];
+  nodeQueue.push(root);
+
+  while (nodeQueue.length) {
+    let node = nodeQueue.pop();
+    arr.unshift(node.val);
+    if (node.left !== null) {
+      nodeQueue.push(node.left);
+    }
+    if (node.right !== null) {
+      nodeQueue.push(node.right);
+    }
+  }
+
+  return arr;
+};
+
+const res2 = postorderTraversal2(makeTreeNodes([1, null, 2, 3]));
+console.log('---', res2);
