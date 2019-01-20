27e96d6d64bdc4f8cbfbd07b802c3fffd372d1d3
denisschmidt <parasolgroup25@gmail.com>
2019-01-20 13:48:30 +0300

    Pow

---
+++ string/50. Pow(x, n)/index.js
@@ -0,0 +1,50 @@
+/*
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+  Input: 2.00000, 10
+  Output: 1024.00000
+
+Example 2:
+  Input: 2.10000, 3
+  Output: 9.26100
+
+Example 3:
+  Input: 2.00000, -2
+  Output: 0.25000
+
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+ */
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+const myPow = function(x, n) {
+  if (n === 0 || x === 1) {
+    return 1;
+  }
+  if (n < 0) {
+    n = -n;
+    x = 1 / x;
+  }
+
+  let ans = 1;
+  while (n > 0) {
+    if (n % 2 === 1) {
+      ans *= x;
+      n--;
+    }
+    x *= x;
+    n /= 2;
+  }
+  return ans;
+};
+
+let x = 2.0,
+  y = 10;
+const res = myPow(x, y);
+
+console.log('---', res, Math.pow(x, y));
