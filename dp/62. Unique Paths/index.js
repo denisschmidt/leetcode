e8bdb8f732f43fc303361922832a496d68320942
denisschmidt <parasolgroup25@gmail.com>
2019-02-24 10:27:47 +0300

    Dp

--- dp/62. Unique Paths/index.js
+++ dp/62. Unique Paths/index.js
@@ -7,3 +7,31 @@ The robot can only move either down or right at any point in time. The robot is 
 How many possible unique paths are there?
 
  */
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const uniquePaths = function(m, n) {
+  const matrix = Array(n)
+    .fill(null)
+    .map(() => {
+      return Array(m).fill(0);
+    });
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (i === 0 || j === 0) {
+        matrix[i][j] = 1;
+      } else {
+        matrix[i][j] = matrix[i][j - 1] + matrix[i - 1][j];
+      }
+    }
+  }
+
+  return matrix[n - 1][m - 1];
+};
+
+const res = uniquePaths(3, 2);
+console.log('---', res);
