22c92104c23939326a8aca7bdda0669dbf85d805
denisschmidt <parasolgroup25@gmail.com>
2019-03-01 09:11:39 +0300

    Array

--- array/41. First Missing Positive/index.js
+++ array/41. First Missing Positive/index.js
@@ -34,3 +34,31 @@ const firstMissingPositive = function(arr) {
 
 const res = firstMissingPositive([1, 2, 0]);
 console.log('===', res);
+
+// =====================================================================================================================
+
+// use swap to put nums[i] to position i - 1, for example put 1 to position 0.
+// using a while in case the new nums[i] is not i - 1, continue put it to right location.
+// check nums[nums[i] - 1] != nums[i] in case they are same number falling to forever loop.
+const firstMissingPositive2 = function(A) {
+  // A[A[i] - 1] !== A[i] checks if index and value of element are in right position.
+  // It's basically to deal with duplicates.
+  for (let i = 0; i < A.length; i++) {
+    while (A[i] > 0 && A[i] - 1 < A.length && A[i] - 1 !== i && A[A[i] - 1] !== A[i]) {
+      // swap
+      let j = A[i] - 1;
+      let tmp = A[i];
+      A[i] = A[j];
+      A[j] = tmp;
+    }
+  }
+
+  let i = 0;
+  for (; i < A.length; i++) {
+    if (i + 1 !== A[i]) break;
+  }
+  return i + 1;
+};
+
+const res2 = firstMissingPositive2([7, 8, 9, 11, 12]);
+console.log('===', res2);
