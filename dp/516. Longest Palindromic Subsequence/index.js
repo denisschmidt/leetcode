e3358e71314e78994ac7bf4d414cc7cf44ff2a65
denisschmidt <parasolgroup25@gmail.com>
2019-03-09 05:47:04 +0300

    DP important task

--- dp/516. Longest Palindromic Subsequence/index.js
+++ dp/516. Longest Palindromic Subsequence/index.js
@@ -43,3 +43,27 @@ const longestPalindromeSubseq = s => {
 
 const res = longestPalindromeSubseq('cbbd');
 console.log('---', res);
+
+const longestPalindromeSubseq2 = s => {
+  const size = s.length;
+  const dp = Array(s.length)
+    .fill(null)
+    .map(() => Array(s.length).fill(0));
+
+  for (let i = size - 1; i >= 0; i--) {
+    for (let j = i; j < size; j++) {
+      if (i === j) {
+        dp[i][j] = 1;
+      } else if (s[i] !== s[j]) {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      } else {
+        dp[i][j] = dp[i + 1][j - 1] + 2;
+      }
+    }
+  }
+
+  return dp[0][size - 1];
+};
+
+const res2 = longestPalindromeSubseq2('cbbd');
+console.log('---', res2);
