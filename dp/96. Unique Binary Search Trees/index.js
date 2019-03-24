73484405f9e728d89cdbc65183a66bc04a553402
denisschmidt <parasolgroup25@gmail.com>
2019-03-24 07:57:25 +0300

    DP Problem Solution Important Task

--- dp/96. Unique Binary Search Trees/index.js
+++ dp/96. Unique Binary Search Trees/index.js
@@ -64,20 +64,22 @@ Given n = 3, there are a total of 5 unique BST's:
     /    \    /    \    /   \
   G(0)  G(2) G(1) G(1) G(2) G(0)
 
-  F(i, n) = G(i - 1) * G(n - i)
+
+  F(i, n) = G(i - 1) * G(n - i) ======>  This is Solution this Problem Main Equation
 
   // 2
-  dp[2] = dp[1] * dp[0]
   dp[2] = dp[0] * dp[1]
+  dp[2] = dp[1] * dp[0]
 
   //3
-  dp[2] = dp[1] * dp[0]
   dp[2] = dp[0] * dp[1]
+  dp[2] = dp[1] * dp[0]
          =====> 2
 
-  dp[3] = dp[2] * dp[0] = 2
-  dp[3] = dp[1] * dp[1] = 3
   dp[3] = dp[0] * dp[2] = 2
+  dp[3] = dp[1] * dp[1] = 3
+  dp[3] = dp[2] * dp[0] = 2
+
           ====> 5
 
  */
@@ -87,21 +89,21 @@ Given n = 3, there are a total of 5 unique BST's:
 // Time Complexity: O(N^2)
 //
 // Space Complexity: O(N)
-const numTrees2 = function(n) {
-  const dp = new Array(n + 1).fill(0);
+const numTrees2 = function(size) {
+  const dp = new Array(size + 1).fill(0);
   dp[0] = dp[1] = 1;
 
   // числа Катлана для 0 и для 1 = 1
   // Дальше расчитываем для 2 и т.д пока не дойдем до нашего числа
 
-  for (let i = 2; i <= n; ++i) {
+  for (let n = 2; n <= size; ++n) {
     for (let j = 1; j <= i; ++j) {
       //  All possible unique left BST's count is G[j - 1] if we plant at i.
       //  All possible unique right BST's count is G[i - j] if we plant at i.
-      dp[i] += dp[i - j] * dp[j - 1];
+      dp[i] += dp[j - 1] * dp[n - j];
     }
   }
-  return dp[n];
+  return dp[size];
 };
 
 const res2 = numTrees2(3);
