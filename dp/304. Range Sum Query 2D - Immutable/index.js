452981a8689e502852d663d03657601a8f86636f
denisschmidt <parasolgroup25@gmail.com>
2019-03-28 07:46:52 +0300

    DP Problem Solution Important Task

---
+++ dp/304. Range Sum Query 2D - Immutable/index.js
@@ -0,0 +1,45 @@
+/*
+Given a 2D matrix matrix,
+find the sum of the elements inside the rectangle
+defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1)
+and (row2, col2) = (4, 3), which contains sum = 8.
+
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+
+ */
+
+/**
+ * @param {number[][]} matrix
+ */
+const NumMatrix = function(matrix) {};
+
+/**
+ * @param {number} row1
+ * @param {number} col1
+ * @param {number} row2
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = Object.create(NumMatrix).createNew(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+
+const matrix = [[], []];
+
+const sumRegion = () => {};
