4a9d4c1e2b9253599443dfde588902e72402372e
denisschmidt <parasolgroup25@gmail.com>
2019-08-04 18:54:19 +0300

    Array -> Backtracking Array -> Sudoku Recursion

---
+++ backtracking/36. Valid Sudoku/index.js
@@ -0,0 +1,108 @@
+/*
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+Example 2:
+
+Input:
+[
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+The given board contain only digits 1-9 and the character '.'.
+The given board size is always 9x9.
+
+ */
+
+const board = [
+  ['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+  ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+  ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+  ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+  ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+  ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+  ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+  ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+  ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+];
+
+const isValidSudoku = function(board, row = 0, col = 0) {
+  if (row === 9) {
+    return true;
+  }
+
+  if (col === 9) {
+    return isValidSudoku(board, row + 1, 0);
+  }
+
+  if (board[row][col] === '.') {
+    return isValidSudoku(board, row, col + 1);
+  }
+
+  // Check current column
+  for (let i = 0; i < 9; i++) {
+    if (board[i][col] === board[row][col] && i !== row) {
+      return false;
+    }
+  }
+
+  // Check current row
+  for (let j = 0; j < 9; j++) {
+    if (board[row][j] === board[row][col] && j !== col) {
+      return false;
+    }
+  }
+
+  // Check current 3x3 grid
+  let si = Math.floor(row / 3) * 3;
+  let sj = Math.floor(col / 3) * 3;
+
+  for (let i = si; i < si + 3; i++) {
+    for (let j = sj; j < sj + 3; j++) {
+      if (board[i][j] === board[row][col] && i !== row && j !== col) {
+        return false;
+      }
+    }
+  }
+
+  return isValidSudoku(board, row, col + 1);
+};
+
+const res = isValidSudoku(board);
+console.log('---', res);
