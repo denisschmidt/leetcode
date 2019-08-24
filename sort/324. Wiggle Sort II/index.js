06b4b2b9a9b1838a56c6fb5c429b768c697f96d2
denisschmidt <parasolgroup25@gmail.com>
2019-08-24 18:11:43 +0300

    Sort -> Quick Sort -> Select Sort

--- sort/324. Wiggle Sort II/index.js
+++ sort/324. Wiggle Sort II/index.js
@@ -18,6 +18,32 @@ Can you do it in O(n) time and/or in-place with O(1) extra space?
 
  */
 
+// Top Solution 1
+// Virtual Indexes
+
+// 1) Use Quick Select Time O(N)
+// 2) Use Three Way Partitioning
+// to arrange the numbers so that those larger than the median come first,
+// then those equal to the median come next, and then those smaller than the median come last.
+
+// https://en.wikipedia.org/wiki/Dutch_national_flag_problem#Pseudocode
+
+//Let's say nums is [10,11,...,19]. Then after quick sort and ordinary partitioning, we might have this (15 is my median):
+
+// index:     0  1  2  3   4   5  6  7  8  9
+// number:   18 17 19 16  15  11 14 10 13 12
+
+// I rewire it so that the first spot has index 5, the second spot has index 0, etc, so that I might get this instead:
+//
+// index:     5  0  6  1  7  2  8  3  9  4
+// number:   11 18 14 17 10 19 13 16 12 15
+// And 11 18 14 17 10 19 13 16 12 15 is perfectly wiggly.
+// And the whole partitioning-to-wiggly-arrangement (everything after finding the median) only takes O(n) time and O(1) space.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Solution 2
+
 // Sort and then write the smaller half of the numbers on the even indexes
 // and the larger half of the numbers on the odd indexes, both from the back.
 
