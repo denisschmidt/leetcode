06345f76900645ec7869f70b44bfc515fe7805d2
denisschmidt <parasolgroup25@gmail.com>
2019-09-15 05:02:19 +0300

    Random Reservoir sampling Shuffle

---
+++ random/398. Random Pick Index/index.js
@@ -0,0 +1,51 @@
+/*
+Given an array of integers with possible duplicates, randomly output the index of a given target number. 
+You can assume that the given target number must exist in the array.
+
+Note:
+The array size can be very large. Solution that uses too much extra space will not pass the judge.
+
+Example:
+  int[] nums = new int[] {1,2,3,3,3};
+  Solution solution = new Solution(nums);
+
+  // pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+  solution.pick(3);
+
+  // pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+  solution.pick(1);
+
+ */
+
+// Наивное решение в котором в память записывается очень большой массив
+// Time O(K) где K кол-вл встречающихся элементов
+// Space O(N)
+class Solution2 {
+  constructor(nums) {
+    this.map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+      let num = nums[i];
+      if (!this.map.has(num)) {
+        this.map.set(num, [i]);
+      } else {
+        this.map.set(num, [...this.map.get(num), i]);
+      }
+    }
+  }
+
+  pick(target) {
+    const arr = this.map.get(target);
+
+    if (arr.length === 1) {
+      return arr[0];
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+      const index = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[index]] = [arr[index], arr[i]];
+    }
+
+    return arr[0];
+  }
+}
