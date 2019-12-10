5f25a4d6b050dd4acd852073fdc724dd02995df4
denisschmidt <parasolgroup25@gmail.com>
2019-12-11 00:38:50 +0300

    Tree DFS

---
+++ 1 hot/536. Construct Binary Tree from String/index.js
@@ -0,0 +1,39 @@
+/*
+You need to construct a binary tree from a string consisting of parenthesis and integers.
+
+The whole input represents a binary tree.
+It contains an integer followed by zero, one or two pairs of parenthesis.
+The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
+
+You always start to construct the left child node of the parent first if it exists.
+
+Example:
+  Input: "4(2(3)(1))(6(5))"
+  Output: return the tree root node representing the following tree:
+
+         4
+       /   \
+      2     6
+     / \   /
+    3   1 5
+
+Note:
+  There will only be '(', ')', '-' and '0' ~ '9' in the input string.
+  An empty tree is represented by "" instead of "()".
+
+
+1(3)(1)
+
+
+ */
+
+const str2tree = s => {};
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
+str2tree('4(2(3)(1))(6(5))');
