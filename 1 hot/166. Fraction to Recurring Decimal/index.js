988d7181b83f8e2b688bda511b4d185f2536f25d
denisschmidt <parasolgroup25@gmail.com>
2019-12-19 00:27:01 +0300

    Math

--- 1 hot/166. Fraction to Recurring Decimal/index.js
+++ 1 hot/166. Fraction to Recurring Decimal/index.js
@@ -16,6 +16,54 @@ Example 3:
   Output: "0.(6)"
  */
 
+// Time O(N)
+// Space O(N)
 const fractionToDecimal = (numerator, denominator) => {
-  // 2857142857142857
+  if (denominator === 0) {
+    return 'NaN';
+  }
+
+  if (numerator === 0) {
+    return '0';
+  }
+
+  // убираем знак операндов
+  const n = Math.abs(numerator);
+  const d = Math.abs(denominator);
+
+  let result = [];
+
+  // определить знак
+  if ((numerator < 0) ^ (denominator < 0)) {
+    result.push('-');
+  }
+
+  // добавить неотъемлемую часть
+  result.push(Math.floor(n / d));
+
+  if (n % d === 0) {
+    return result.join('');
+  }
+
+  result.push('.');
+
+  let map = new Map();
+
+  for (let r = n % d; r > 0; r %= d) {
+    if (map.has(r)) {
+      result.splice(map.get(r), 0, '(');
+      result.push(')');
+      break;
+    }
+
+    // остаток виден первым
+    // запоминаем текущую позицию для нее
+    map.set(r, result.length);
+
+    r *= 10;
+
+    result.push(Math.floor(r / d));
+  }
+
+  return result.join('');
 };
