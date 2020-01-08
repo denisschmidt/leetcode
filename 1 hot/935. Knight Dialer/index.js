90f7cabccd128c27c9d61705d3c2cea241c0b3f2
denisschmidt <parasolgroup25@gmail.com>
2020-01-08 23:55:27 +0300

    DP knapsack problem

---
+++ 1 hot/935. Knight Dialer/index.js
@@ -0,0 +1,93 @@
+/*
+A chess knight can move as indicated in the chess diagram below:
+
+This time, we place our chess knight on any numbered key of a phone pad (indicated above), and the knight makes N-1 hops.
+Each hop must be from one key to another numbered key.
+
+Each time it lands on a key (including the initial placement of the knight), 
+it presses the number of that key, pressing N digits total.
+
+How many distinct numbers can you dial in this manner?
+
+Since the answer may be large, output the answer modulo 10^9 + 7.
+
+Example 1:
+  Input: 1
+  Output: 10
+
+Example 2:
+  Input: 2
+  Output: 20
+
+Example 3:
+  Input: 3
+  Output: 46
+ 
+
+Note: 1 <= N <= 5000
+
+*/
+
+let mod = 1e9 + 7;
+let neighbors = {
+  '1': ['8', '6'],
+  '2': ['7', '9'],
+  '3': ['4', '8'],
+  '4': ['0', '9', '3'],
+  '5': [],
+  '6': ['1', '7', '0'],
+  '7': ['2', '6'],
+  '8': ['1', '3'],
+  '9': ['4', '2'],
+  '0': ['4', '6'],
+};
+
+const knightDialer = N => {
+  // Наш базовый случай - 1 прыжок для каждого возможного числа
+  let currentCounts = Array.from({ length: 10 }, () => 1);
+
+  for (let hops = 1; hops < N; hops++) {
+    let nextCounts = Array.from({ length: 10 }, () => 0);
+
+    for (let number = 0; number < 10; number++) {
+      for (let neighbor of neighbors[number]) {
+        nextCounts[neighbor] += currentCounts[number] % mod;
+      }
+    }
+    currentCounts = nextCounts;
+  }
+
+  return currentCounts.reduce((acc, v) => acc + v, 0) % mod;
+};
+
+// Time O(N)
+// Space O(N)
+const knightDialer_II = N => {
+  let cnt = 0;
+  let cache = {};
+
+  for (let i = 0; i < 10; i++) {
+    cnt += helper(i, N);
+  }
+  return cnt % mod;
+
+  function helper(position, hops) {
+    let key = [position, hops].join('|');
+
+    if (!cache[key]) {
+      if (hops === 1) {
+        return 1;
+      }
+
+      let ways = 0;
+
+      for (let neighbor of neighbors[position]) {
+        ways += helper(neighbor, hops - 1);
+      }
+
+      cache[key] = ways % mod;
+    }
+
+    return cache[key];
+  }
+};
---
+++ dp/377. Combination Sum IV/index.js
@@ -0,0 +1,126 @@
+/*
+Given an integer array with all positive numbers and no duplicates,
+find the number of possible combinations that add up to a positive integer target.
+
+Example:
+
+  nums = [1, 2, 3]
+  target = 4
+
+  The possible combination ways are:
+  (1, 1, 1, 1)
+  (1, 1, 2)
+  (1, 2, 1)
+  (1, 3)
+  (2, 1, 1)
+  (2, 2)
+  (3, 1)
+
+Note that different sequences are counted as different combinations.
+
+Therefore the output is 7.
+
+Follow up:
+  What if negative numbers are allowed in the given array?
+  How does it change the problem?
+  What limitation we need to add to the question to allow negative numbers?
+
+Credits:
+  Special thanks to @pbrother for adding this problem and creating all test cases.
+
+ */
+
+// Time O(N)
+// Space O(N)
+const combinationSum4 = (nums, target) => {
+  let dp = Array(target + 1).fill(0);
+  dp[0] = 1;
+
+  for (let sum = 1; sum <= target; sum++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] <= sum) {
+        dp[i] += dp[sum - nums[j]];
+      }
+    }
+  }
+
+  return dp[target];
+};
+
+/*
+  Backtracking !!!!!!!
+
+  Only for positive number
+
+  Memory Limit Exceeded
+
+  If this problem requires you output the combinations, the backtracking method is very useful.
+
+ */
+const combinationSum4_II = function(nums, target) {
+  const ans = [];
+  combination(ans, [], target);
+  return ans.length;
+
+  function combination(ans, comb, sum = 0) {
+    if (sum < 0) {
+      return;
+    } else if (sum === 0) {
+      ans.push([...comb]);
+      return;
+    } else {
+      for (let i = 0; i < nums.length; i++) {
+        comb.push(nums[i]);
+        combination(ans, comb, sum - nums[i]);
+        comb.pop();
+      }
+    }
+  }
+};
+
+/*
+  Recursion
+ */
+const combinationSum4Reqursion = function(nums, target) {
+  if (target === 0) {
+    return 1;
+  }
+  let res = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (target >= nums[i]) {
+      res += combinationSum4Reqursion(nums, target - nums[i]);
+    }
+  }
+  return res;
+};
+
+const combinationSum4DFS = function(nums, target) {
+  if (!nums.length) {
+    return 0;
+  }
+  const map = new Map();
+
+  const dfs = (nums, sum, map) => {
+    if (sum === 0) {
+      return 1;
+    }
+
+    if (sum < 0) {
+      return 0;
+    }
+
+    if (map.has(sum)) {
+      return map.get(sum);
+    }
+
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+      count += dfs(nums, sum - nums[i], map);
+    }
+
+    map.set(sum, count);
+    return count;
+  };
+
+  return dfs(nums, target, map);
+};
