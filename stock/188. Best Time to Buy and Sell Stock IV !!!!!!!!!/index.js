8d04b64283e34ba8ab19805cc9a6a98da605acd8
denisschmidt <parasolgroup25@gmail.com>
2020-01-15 03:21:56 +0300

    DP -> Stocks Problems

--- stock/188. Best Time to Buy and Sell Stock IV !!!!!!!!!/index.js
+++ stock/188. Best Time to Buy and Sell Stock IV !!!!!!!!!/index.js
@@ -18,10 +18,47 @@ Example 2:
   Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
 
-
  */
 
-const maxProfit = (k, prices) => {};
+// Time O(N*K)
+// Space O(K)
+const maxProfit = (k, prices) => {
+  /*
+    Выгодная сделка занимает не менее двух дней (покупка в один день и продажа в другой при условии, что цена покупки меньше цены продажи). 
+    
+    Если длина массива цен равна n, максимальное количество прибыльных транзакций равно n / 2 (целочисленное деление). 
+    После этого прибыльная сделка невозможна, то есть максимальная прибыль останется прежней. 
+    
+    Следовательно, критическое значение k равно n / 2. 
+    
+    Если заданное k не меньше этого значения, то есть k> = n / 2, мы можем расширить k до положительной бесконечности, 
+    и задача эквивалентна случаю II.
+
+  */
+  if (k >= prices.length >> 1) {
+    let price_0 = 0;
+    let price_1 = -Number.MAX_VALUE;
+
+    for (const price of prices) {
+      price_0 = Math.max(price_0, price_1 + price);
+      price_1 = Math.max(price_1, price_0 - price);
+    }
+
+    return price_0;
+  }
+
+  let dp_0 = Array(k + 1).fill(0);
+  let dp_1 = Array(k + 1).fill(-Number.MAX_VALUE);
+
+  for (const price of prices) {
+    for (let i = k; i > 0; i--) {
+      dp_0[i] = Math.max(dp_0[i], dp_1[i] + price); // холд или покупка
+      dp_1[i] = Math.max(dp_1[i], dp_0[i - 1] - price); // холд или продажа
+    }
+  }
+
+  return dp_0[k];
+};
 
 // The key point is when there are two v/p pairs (v1, p1) and (v2, p2), satisfying
 // v1 <= v2 and p1 <= p2, we can either make one transaction at [v1, p2], or make
