0d46a7cb28543f751e09ef670594b9ea9e49d006
denisschmidt <parasolgroup25@gmail.com>
2020-02-03 23:09:49 +0300

    matrix

---
+++ dfs bfs/1222. Queens That Can Attack the King/index.js
@@ -0,0 +1,98 @@
+/*
+
+On an 8x8 chessboard, there can be multiple Black Queens and one White King.
+
+Given an array of integer coordinates queens that represents the positions of the Black Queens,
+and a pair of coordinates king that represent the position of the White King, 
+return the coordinates of all the queens (in any order) that can attack the King.
+ 
+Example 1:
+  Input: queens = [[0,1],[1,0],[4,0],[0,4],[3,3],[2,4]], king = [0,0]
+  Output: [[0,1],[1,0],[3,3]]
+  Explanation:  
+    The queen at [0,1] can attack the king cause they're in the same row. 
+    The queen at [1,0] can attack the king cause they're in the same column. 
+    The queen at [3,3] can attack the king cause they're in the same diagnal. 
+    The queen at [0,4] can't attack the king cause it's blocked by the queen at [0,1]. 
+    The queen at [4,0] can't attack the king cause it's blocked by the queen at [1,0]. 
+    The queen at [2,4] can't attack the king cause it's not in the same row/column/diagnal as the king.
+
+Example 2:
+  Input: queens = [[0,0],[1,1],[2,2],[3,4],[3,5],[4,4],[4,5]], king = [3,3]
+  Output: [[2,2],[3,4],[4,4]]
+
+Example 3:
+  Input: queens = [[5,6],[7,7],[2,1],[0,7],[1,6],[5,1],[3,7],[0,3],[4,0],[1,2],[6,3],[5,0],[0,4],[2,2],[1,1],[6,4],[5,4],[0,0],[2,6],[4,5],[5,2],[1,4],[7,5],[2,3],[0,5],[4,2],[1,0],[2,7],[0,1],[4,6],[6,1],[0,6],[4,3],[1,7]], king = [3,4]
+  Output: [[2,3],[1,4],[1,6],[3,7],[4,3],[5,4],[4,5]]
+ 
+
+Constraints:
+  1 <= queens.length <= 63
+  queens[0].length == 2
+  0 <= queens[i][j] < 8
+  king.length == 2
+  0 <= king[0], king[1] < 8
+  At most one piece is allowed in a cell.
+
+*/
+
+// Time O(N^2)
+// Space O(N^2)
+const queensAttacktheKing = function(queens, king) {
+  let comb = [];
+  let dirs = [
+    [1, 0],
+    [0, -1],
+    [0, 1],
+    [-1, 0],
+  ];
+
+  let diags = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1],
+  ];
+
+  let set = new Set();
+  let maxY = 8;
+  let maxX = 8;
+
+  for (let i = 0; i < queens.length; i++) {
+    let [s, e] = queens[i];
+    let key = `${s}_${e}`;
+    set.add(key);
+  }
+
+  for (let dir of dirs) {
+    let x = king[0];
+    let y = king[1];
+
+    while (x >= 0 && x <= maxY && y >= 0 && y <= maxX) {
+      x += dir[0];
+      y += dir[1];
+
+      if (set.has(`${x}_${y}`)) {
+        comb.push([x, y]);
+        break;
+      }
+    }
+  }
+
+  for (let diag of diags) {
+    let x = king[0];
+    let y = king[1];
+
+    while (x >= 0 && x <= maxY && y >= 0 && y <= maxX) {
+      x += diag[0];
+      y += diag[1];
+
+      if (set.has(`${x}_${y}`)) {
+        comb.push([x, y]);
+        break;
+      }
+    }
+  }
+
+  return comb;
+};
