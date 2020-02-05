749aef2c3af520ab4b3d38e48969b7cef12fe196
denisschmidt <denis.samsonov@aliexpress.ru>
2020-02-05 12:14:47 +0300

    array

---
+++ 1 now/index.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/*
+
+  [10,2,8,9,3,8,1,5,2,3,7,6]
+
+  {
+  '1': 1,
+  '2': 2,
+  '3': 2,
+  '5': 1,
+  '6': 1,
+  '7': 1,
+  '8': 2,
+  '9': 1,
+  '10': 1
+}
+
+*/
+
+var maxEqualFreq = function(nums) {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    map[nums[i]] = ~~map[nums[i]] + 1;
+  }
+
+  let max = 0;
+  let min = Number.MAX_VALUE;
+
+  Object.keys(map).forEach(key => {
+    max = Math.max(max, map[key]);
+    min = Math.min(min, map[key]);
+  });
+
+  if (max === min) {
+    return nums.length - 1;
+  }
+
+  console.log(map);
+};
+
+maxEqualFreq([10, 2, 8, 9, 3, 8, 1, 5, 2, 3, 7, 6]);
