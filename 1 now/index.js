5d1699357d61f58380b3c35208e7b0a74e347797
denisschmidt <parasolgroup25@gmail.com>
2020-02-25 21:01:50 +0300

    stack

--- 1 now/index.js
+++ 1 now/index.js
@@ -1,51 +1,50 @@
 /*
-
-  [2, 6, 4, 8, 10, 9, 15]
-
-  [6, 4, 8, 10, 9]
+  [2,-1,2] 3
 
 
-  [1, 3, 2, 2, 2]
- [1,3,2,2,2]
+  1, 2, [3,4,5] 10  [3, 4, 5] -> 12  left = 3 
+
+  1, 3 [6,10,15,] 25
 
 */
 
+// 862. Shortest Subarray with Sum at Least K
+
 /**
- * @param {number[]} nums
+ * @param {number[]} A
+ * @param {number} K
  * @return {number}
  */
-var findUnsortedSubarray = function(nums) {
-  let start = Number.MAX_VALUE;
-  let end = -Number.MAX_VALUE;
+var shortestSubarray = function(A, K) {
+  let prefix = [A[0]];
 
-  if (isValid(nums)) return 0;
+  for (let i = 1; i < A.length; i++) {
+    prefix[i] = prefix[i - 1] + A[i];
+  }
 
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i - 1] > nums[i]) {
-      start = Math.min(start, i - 1);
-      end = Math.max(end, i);
+  for (let i = 0; i < A.length; i++) {}
+};
 
-      swap(nums, i - 1, i);
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+var shortestSubarray_II = function(A, K) {
+  let len = A[0] >= K ? 1 : Number.MAX_VALUE;
 
-      if (isValid(nums)) {
-        return end - start + 1;
+  for (let i = 0; i < A.length; i++) {
+    let sum = A[i];
+    if (sum >= K) {
+      len = Math.min(len, 1);
+    }
+    for (let j = i + 1; j < A.length; j++) {
+      sum += A[j];
+      if (sum >= K) {
+        len = Math.min(len, j - i + 1);
       }
     }
   }
 
-  return end - start + 1;
+  return len === Number.MAX_VALUE ? -1 : len;
 };
-
-function swap(nums, i, j) {
-  return ([nums[i], nums[j]] = [nums[j], nums[i]]);
-}
-
-function isValid(nums) {
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i - 1] > nums[i]) return false;
-  }
-  return true;
-}
-
-let a = findUnsortedSubarray([1, 2, 3, 3, 3]);
-console.log(a);
