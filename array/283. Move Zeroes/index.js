06a07409691cfc7829e35efbbca05de8c6fee410
denisschmidt <parasolgroup25@gmail.com>
2020-04-04 12:24:15 +0300

    leetcode challenge

--- array/283. Move Zeroes/index.js
+++ array/283. Move Zeroes/index.js
@@ -1,35 +1,44 @@
 /*
+
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 Example:
-
-Input: [0,1,0,3,12]
-Output: [1,3,12,0,0]
-Note:
-
-You must do this in-place without making a copy of the array.
-Minimize the total number of operations.
+  Input: [0,1,0,3,12]
+  Output: [1,3,12,0,0]
+  Note:
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
 
  */
 
-/**
- * @param {number[]} nums
- * @return
- */
-const moveZeroes = function(nums) {
-  let left = 0,
-    index = 0;
-  while (left < nums.length) {
-    if (nums[left] !== 0) {
-      let temp = nums[index];
-      nums[index] = nums[left];
-      nums[left] = temp;
-      index++;
+// Time O(N)
+// Space O(1)
+const moveZeroes = nums => {
+  let n = nums.length;
+  let cnt = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] == 0) {
+      cnt++;
     }
-    left++;
+
+    let j = i;
+    while (cnt > 0 && j < n) {
+      if (nums[j] != 0) {
+        swap(nums, i, j);
+        cnt--;
+      }
+      j++;
+    }
+
+    if (j == n) {
+      break;
+    }
   }
 
-  console.log(nums);
+  return nums;
 };
 
-moveZeroes([0, 1, 0, 3, 12]);
+function swap(nums, i, j) {
+  return ([nums[i], nums[j]] = [nums[j], nums[i]]);
+}
