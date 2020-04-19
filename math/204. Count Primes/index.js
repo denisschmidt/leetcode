6bd1a68f2bb0a72ac928dc24130526b6293c056c
denisschmidt <parasolgroup25@gmail.com>
2020-04-19 15:10:46 +0300

    search

--- math/204. Count Primes/index.js
+++ math/204. Count Primes/index.js
@@ -1,3 +1,14 @@
+/*
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+  Input: 10
+  Output: 4
+  Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+*/
+
 // Time O(Nlog(N))
 // Space O(N)
 const countPrimes = n => {
