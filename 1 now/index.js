95f37b5fd7264d6ee4577fae5b1bb573774aeadb
denisschmidt <parasolgroup25@gmail.com>
2020-04-20 12:39:47 +0300

    stack

--- 1 now/index.js
+++ 1 now/index.js
@@ -1,27 +1,11 @@
 const { PriorityQueue } = require('../algorithms/priorityQueue');
 
-var nthSuperUglyNumber = function(n, primes) {
-  let pq = new PriorityQueue({ comparator: (a, b) => a - b });
-  let nums = [];
-  let i = 0;
+/**
+ * @param {number} n
+ * @param {string[]} logs
+ * @return {number[]}
+ */
+var exclusiveTime = function(n, logs) {};
 
-  pq.offer(1);
-
-  while (i < n) {
-    let val = pq.poll();
-
-    for (let x of primes) {
-      pq.offer(x * val);
-    }
-
-    if (nums[nums.length - 1] != val) {
-      nums.push(val);
-      i++;
-    }
-  }
-
-  return nums[n - 1];
-};
-
-let ans = nthSuperUglyNumber(12, [2, 7, 13, 19]);
-console.log(ans);
+let a = exclusiveTime(1, ['0:start:0', '0:start:2', '0:end:5', '0:start:6', '0:end:6', '0:end:7']);
+console.log(a);
