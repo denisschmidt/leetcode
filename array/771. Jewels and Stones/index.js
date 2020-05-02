74b3869eb4354de796777ef9ce99e302e73354c8
denisschmidt <parasolgroup25@gmail.com>
2020-05-02 14:58:42 +0300

    math

--- array/771. Jewels and Stones/index.js
+++ array/771. Jewels and Stones/index.js
@@ -1,4 +1,5 @@
 /*
+
 You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
 Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
@@ -6,34 +7,33 @@ The letters in J are guaranteed distinct, and all characters in J and S are lett
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
 Example 1:
+  Input: J = "aA", S = "aAAbbbb"
+  Output: 3
 
-Input: J = "aA", S = "aAAbbbb"
-Output: 3
 Example 2:
+  Input: J = "z", S = "ZZ"
+  Output: 0
 
-Input: J = "z", S = "ZZ"
-Output: 0
 Note:
-
-S and J will consist of letters and have length at most 50.
-The characters in J are distinct.
+  S and J will consist of letters and have length at most 50.
+  The characters in J are distinct.
 
  */
+
 // Time O(N)
 // Space O(N)
-const numJewelsInStones = function(J, S) {
-  const map = new Map();
-  for (let i = 0; i < S.length; i++) {
-    let char = S[i];
-    if (map.has(char)) {
-      map.set(char, map.get(char) + 1);
-    } else {
-      map.set(char, 1);
+const numJewelsInStones = (J, S) => {
+  let map = new Set();
+
+  for (let x of J) {
+    if (!map.has(x)) map.add(x);
+  }
+
+  let cnt = 0;
+  for (let x of S) {
+    if (map.has(x)) {
+      cnt++;
     }
   }
-  let count = 0;
-  for (let i = 0; i < J.length; i++) {
-    if (map.has(J[i])) count += map.get(J[i]);
-  }
-  return count;
+  return cnt;
 };
