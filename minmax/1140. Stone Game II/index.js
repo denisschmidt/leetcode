ecbfcfd4c8bd7c2866a54aa081f81f7b75187b10
denisschmidt <parasolgroup25@gmail.com>
2020-05-12 07:53:59 +0300

    minmax

--- minmax/1140. Stone Game II/index.js
+++ minmax/1140. Stone Game II/index.js
@@ -27,3 +27,51 @@ Constraints:
   1 <= piles[i] <= 10 ^ 4
 
 */
+
+// MINMAX
+const stoneGameII = piles => {
+  let n = piles.length;
+
+  let INF = Number.MAX_VALUE;
+
+  let dp = Array(n + 1)
+    .fill(0)
+    .map(() =>
+      Array(n + 1)
+        .fill(0)
+        .map(() => [-INF, INF]),
+    );
+
+  return helper(0, 1, true);
+
+  function helper(start, m, isFirst) {
+    if (start >= n) return 0;
+
+    let id = isFirst ? 0 : 1;
+
+    if (id == 0 && dp[start][m][id] != -INF) {
+      return dp[start][m][id];
+    }
+
+    if (id == 1 && dp[start][m][id] != INF) {
+      return dp[start][m][id];
+    }
+
+    if (isFirst) {
+      let x = -INF;
+      for (let i = 1; i <= 2 * m; i++) {
+        let sum = piles.slice(start, start + i).reduce((acc, v) => acc + v, 0);
+        x = Math.max(x, sum + helper(start + i, Math.max(m, i), !isFirst));
+      }
+      dp[start][m][id] = Math.max(dp[start][m][id], x);
+    } else {
+      let x = INF;
+      for (let i = 1; i <= 2 * m; i++) {
+        x = Math.min(x, helper(start + i, Math.max(m, i), !isFirst));
+      }
+      dp[start][m][id] = Math.min(dp[start][m][id], x);
+    }
+
+    return dp[start][m][id];
+  }
+};
