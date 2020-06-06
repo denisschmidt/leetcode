d71d260d8c406ea0ca1e83e82a11ab823961d015
denisschmidt <parasolgroup25@gmail.com>
2020-06-06 17:59:41 +0300

    string

---
+++ string/1433. Check If a String Can Break Another String/index.js
@@ -0,0 +1,52 @@
+/*
+
+Given two strings: s1 and s2 with the same size, check if some permutation of string s1 can break some permutation of string s2 or vice-versa (in other words s2 can break s1).
+
+A string x can break string y (both of size n) if x[i] >= y[i] (in alphabetical order) for all i between 0 and n-1.
+
+Example 1:
+  Input: s1 = "abc", s2 = "xya"
+  Output: true
+  Explanation: "ayx" is a permutation of s2="xya" which can break to string "abc" which is a permutation of s1="abc".
+
+Example 2:
+  Input: s1 = "abe", s2 = "acd"
+  Output: false 
+  Explanation: All permutations for s1="abe" are: "abe", "aeb", "bae", "bea", "eab" and "eba" and all permutation for s2="acd" are: "acd", "adc", "cad", "cda", "dac" and "dca". However, there is not any permutation from s1 which can break some permutation from s2 and vice-versa.
+
+Example 3:
+  Input: s1 = "leetcodee", s2 = "interview"
+  Output: true
+ 
+
+Constraints:
+  s1.length == n
+  s2.length == n
+  1 <= n <= 10^5
+  All strings consist of lowercase English letters.
+
+*/
+
+// Time O(NLogN)
+// Space O(N)
+const checkIfCanBreak = (s1, s2) => {
+  s1 = helper(s1);
+  s2 = helper(s2);
+
+  return canBreak(s1, s2) || canBreak(s2, s1);
+
+  function canBreak(s1, s2) {
+    let cnt = 0;
+    for (let i = 0; i < s1.length; i++) {
+      if (s1[i] >= s2[i]) cnt++;
+    }
+    return cnt == s1.length;
+  }
+
+  function helper(str) {
+    return str
+      .split('')
+      .sort()
+      .join('');
+  }
+};
