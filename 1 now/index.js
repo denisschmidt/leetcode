7cc4fa7ed2a1bce87fa7955d5a803f4cec4fa6c6
denisschmidt <parasolgroup25@gmail.com>
2020-06-06 11:13:00 +0300

    array

--- 1 now/index.js
+++
@@ -1,9 +0,0 @@
-const parseUrl = url => {
-  url = url.split('/');
-
-  console.log(url);
-
-  console.log('http://news.yahoo.com/news/topics/'.startsWith('http://news.yahoo.com'));
-};
-
-parseUrl('http://news.yahoo.com/news/topics/');
