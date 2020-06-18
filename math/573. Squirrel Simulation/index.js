b7341195bf5be91dd7aa8973d474091c5d3da2e5
denisschmidt <parasolgroup25@gmail.com>
2020-06-18 17:33:07 +0300

    math

---
+++ math/573. Squirrel Simulation/index.js
@@ -0,0 +1,50 @@
+/*
+
+There's a tree, a squirrel, and several nuts. 
+
+Positions are represented by the cells in a 2D grid. 
+
+Your goal is to find the minimal distance for the squirrel to collect all the nuts and put them under the tree one by one. The squirrel can only take at most one nut at one time and can move in four directions - up, down, left and right, to the adjacent cell. 
+
+The distance is represented by the number of moves.
+
+Example 1:
+  Input: 
+  Height : 5
+  Width : 7
+  Tree position : [2,2]
+  Squirrel : [4,4]
+  Nuts : [[3,0], [2,5]]
+  Output: 12
+  Explanation:
+  ​​​​​
+Note:
+  All given positions won't overlap.
+  The squirrel can take at most one nut at one time.
+  The given positions of nuts have no order.
+  Height and width are positive integers. 3 <= height * width <= 10,000.
+  The given positions contain at least one nut, only one tree and one squirrel.
+
+*/
+
+// Time O(N)
+// Space O(1)
+const minDistance = (height, width, tree, squirrel, nuts) => {
+  let max = -Number.MAX_VALUE;
+  let sum = 0;
+
+  for (let nut of nuts) {
+    let d1 = getDist(nut, tree);
+    let d2 = getDist(nut, squirrel);
+
+    sum += 2 * d1;
+
+    max = Math.max(max, d1 - d2);
+  }
+
+  return sum - max;
+
+  function getDist([x, y], [u, z]) {
+    return Math.abs(x - u) + Math.abs(y - z);
+  }
+};
