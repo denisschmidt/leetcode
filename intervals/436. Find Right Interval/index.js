4a03c92916c64a3a831704c11a75b028964df2a6
denisschmidt <parasolgroup25@gmail.com>
2020-07-05 12:35:18 +0300

    array

---
+++ intervals/436. Find Right Interval/index.js
@@ -0,0 +1,69 @@
+/*
+
+Given a set of intervals, for each of the interval i, check if there exists an interval j whose start point is bigger than or equal to the end point of the interval i, which can be called that j is on the "right" of i.
+
+For any interval i, you need to store the minimum interval j's index, which means that the interval j has the minimum start point to build the "right" relationship for interval i. 
+
+If the interval j doesn't exist, store -1 for the interval i. 
+
+Finally, you need output the stored value of each interval as an array.
+
+Note:
+  You may assume the interval's end point is always bigger than its start point.
+  You may assume none of these intervals have the same start point.
+ 
+
+Example 1:
+  Input: [ [1,2] ]
+  Output: [-1]
+  Explanation: There is only one interval in the collection, so it outputs -1.
+ 
+
+Example 2:
+  Input: [ [3,4], [2,3], [1,2] ]
+  Output: [-1, 0, 1]
+  Explanation: There is no satisfied "right" interval for [3,4].
+  For [2,3], the interval [3,4] has minimum-"right" start point;
+  For [1,2], the interval [2,3] has minimum-"right" start point.
+ 
+
+Example 3:
+  Input: [ [1,4], [2,3], [3,4] ]
+  Output: [-1, 2, -1]
+  Explanation: There is no satisfied "right" interval for [1,4] and [3,4].
+  For [2,3], the interval [3,4] has minimum-"right" start point.
+
+NOTE: input types have been changed on April 15, 2019. 
+Please reset to default code definition to get new method signature.
+
+*/
+
+// Time O(NLogN + N^2)
+// Space O(N)
+const findRightInterval = function(intervals) {
+  let n = intervals.length;
+  let res = Array(n).fill(-1);
+
+  intervals = intervals.map(([a, b], index) => [a, b, index]);
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 0; i < n; i++) {
+    let j = i + 1;
+    let found = null;
+
+    while (j < n) {
+      if (intervals[i][1] <= intervals[j][0]) {
+        found = intervals[j];
+        break;
+      }
+      j++;
+    }
+    if (found) {
+      res[intervals[i][2]] = intervals[j][2];
+    } else {
+      res[intervals[i][2]] = -1;
+    }
+  }
+
+  return res;
+};
