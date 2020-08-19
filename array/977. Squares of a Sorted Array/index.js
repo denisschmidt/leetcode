eccf70dd6c433b2f973986e32498150dd9a166cb
denisschmidt <parasolgroup25@gmail.com>
2020-08-19 19:06:23 +0300

    string

--- array/977. Squares of a Sorted Array/index.js
+++ array/977. Squares of a Sorted Array/index.js
@@ -1,6 +1,6 @@
 /*
-Given an array of integers A sorted in non-decreasing order,
-return an array of the squares of each number, also in sorted non-decreasing order.
+
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
 
 Example 1:
   Input: [-4,-1,0,3,10]
@@ -15,15 +15,10 @@ Note:
   -10000 <= A[i] <= 10000
   A is sorted in non-decreasing order.
 
- */
+*/
 
-/**
- * @param {number[]} A
- * @return {number[]}
- */
-var sortedSquares = function(A) {
-  return A.map(a => Math.pow(a, 2)).sort((a, b) => a - b);
+// Time O(NLogN)
+// Space O(N)
+const sortedSquares = A => {
+  return A.sort((a, b) => Math.abs(a) - Math.abs(b)).map(x => x * x);
 };
-
-const res = sortedSquares([-4, -1, 0, 3, 10]);
-console.log('--', res);
---
+++ string/791. Custom Sort String/py/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def customSortString(self, S: str, T: str) -> str:
+      count = collections.Counter(T);
+      ans = []
+
+      # Write all characters that occur in S, in the order of S.  
+      for c in S:
+          ans.append(c * count[c])
+          # Set count[c] = 0 to denote that we do not need
+          # to write 'c' to our answer anymore.
+          count[c] = 0
+          
+      # Write all remaining characters that don't occur in S.
+      # That information is specified by 'count'.    
+      for c in count:
+          ans.append(c * count[c])
+          
+      return ''.join(ans)    
