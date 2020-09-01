2c371636f420462b5fbb978bd471e03e0c953d6d
denisschmidt <parasolgroup25@gmail.com>
2020-09-01 05:58:37 +0300

    dp

--- stock/123. Best Time to Buy and Sell Stock III/index.js
+++ stock/123. Best Time to Buy and Sell Stock III/index.js
@@ -26,22 +26,70 @@ Example 3:
 
 */
 
+/*
+  
+  Bidirectional Dynamic Programming
+  We can divide this problem into two subproblems
+  Each subproblem is actually of the same problem of "Best Time to Buy and Sell Stock"
+
+  A naive implementation: Time O(N^2)
+  We can divide the sequences into two after enumerate each of the subsequences and use "Best Time to Buy and Sell Stock" solution
+
+  Using bidirectional dynamic programming.
+ 
+  In this problem though, we would use two arrays -> left_profits and right_profits
+
+  Left_profits[i] - hold the maximum profits that one can gain from doing one single transaction on the left subsequence of prices from the index zero to i
+  Right_profits[i] - hold the maximum profits that one can gain from doing one single transaction on the right subsequence of the prices from the index i up to N-1
+
+  Now, if we divide the sequence of prices around the element at the index i, into two subsequences 
+  With left subsequences as Prices[0], Prices[1], ... Prices[i] and the right subsequence as Prices[i+1], ... Prices[N-1]
+  The total maximum profits that we obtain from this division (denoted as max_profits[i]) can be expressed as follows
+  max_profits[i] = left_profits[i] + right_profits[i]
+
+
+  Stocks: [7, 1, 5, 3, 6, 4]
+  Left_Profits:   [0, 0, 4, 4, 5, 5]
+  Right_Profits:  [5, 5, 3, 3, 0, 0]
+
+*/
+
 // Time O(N)
 // Space O(N)
 const maxProfit = prices => {
-  let k = 2;
-  let price_0 = Array(k + 1).fill(0);
-  let price_1 = Array(k + 1).fill(-Number.MAX_VALUE);
+  let n = prices.length;
+  let left_profits = Array(n).fill(0);
+  let max = 0;
+  let INF = Number.MAX_VALUE;
+  let left_min = INF;
 
-  for (let price of prices) {
-    price_0[2] = Math.max(price_0[2], price_1[2] + price);
-    price_1[2] = Math.max(price_1[2], price_0[1] - price);
-
-    price_0[1] = Math.max(price_0[1], price_1[1] + price);
-    price_1[1] = Math.max(price_1[1], price_0[0] - price);
+  for (let i = 0; i < n; i++) {
+    if (left_min > prices[i]) {
+      left_min = prices[i];
+    }
+    max = Math.max(max, prices[i] - left_min);
+    left_profits[i] = max;
   }
 
-  return price_0[2];
+  let right_max = -INF;
+  let right_profits = Array(n).fill(0);
+  max = 0;
+
+  for (let i = n - 1; i >= 0; i--) {
+    if (prices[i] > right_max) {
+      right_max = prices[i];
+    }
+    max = Math.max(max, right_max - prices[i]);
+    right_profits[i] = max;
+  }
+
+  let res = 0;
+
+  for (let i = 0; i < n; i++) {
+    res = Math.max(res, left_profits[i] + right_profits[i]);
+  }
+
+  return res;
 };
 
 // Time O(2N)
