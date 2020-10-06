166cc7ea12ecec15508192f5aec1f872ca7f6049
denisschmidt <parasolgroup25@gmail.com>
2020-10-07 02:20:01 +0300

    bst

--- tree/701. Insert into a Binary Search Tree/js/main.js
+++ tree/701. Insert into a Binary Search Tree/js/main.js
@@ -1,36 +1,5 @@
 /*
 
-Given the root node of a binary search tree (BST) and a value to be inserted into the tree,
-insert the value into the BST.
-Return the root node of the BST after the insertion.
-It is guaranteed that the new value does not exist in the original BST.
-
-Note that there may exist multiple valid ways for the insertion,
-as long as the tree remains a BST after insertion.
-You can return any of them.
-
-For example,
-
-Given the tree:
-      4
-     / \
-    2   7
-   / \
-  1   3
-And the value to insert: 5
-
-
-You can return this binary search tree:
-      4
-     /   \
-    2      7
-   / \    /
-  1   3  5
-
-*/
-
-/*
-
   Алгоритм:
 
   Реализация рекурсии очень проста:     
@@ -42,12 +11,16 @@ You can return this binary search tree:
 
 // Time O(H) - Average case Time O(LogN) - Worst case O(N)
 // Space O(H) - Average case Time O(LogN) - Worst case O(N)
-const insertIntoBST = (node, target) => {
-  if (node == null) return new TreeNode(target);
+const insertIntoBST = (root, val) => {
+  if (root == null) {
+    return new TreeNode(val);
+  }
 
-  if (target > node.val) node.right = insertIntoBST(node.right);
+  if (root.val > val) {
+    root.left = insertIntoBST(root.left, val);
+  } else {
+    root.right = insertIntoBST(root.right, val);
+  }
 
-  if (target < node.val) node.left = insertIntoBST(node.left);
-
-  return node;
+  return root;
 };
