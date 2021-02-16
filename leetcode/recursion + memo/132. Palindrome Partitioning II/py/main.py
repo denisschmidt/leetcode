2c86016cff8977de9245f2014dde200ebb3e2c58
denisschmidt <parasolgroup25@gmail.com>
2021-02-16 09:52:11 +0300

    daily edu

--- leetcode/recursion + memo/132. Palindrome Partitioning II/py/main.py
+++ leetcode/recursion + memo/132. Palindrome Partitioning II/py/main.py
@@ -1,31 +1,68 @@
-# Top - Down 
-# DFS + Memo
-class Solution:
-    def minCut(self, s: str) -> int:
-        n = len(s)
+from functools import lru_cache
 
-        # Double memo 
-        dp = [None] * n 
+
+class Solution:
+    # Time O(N^2) Precompute DP
+    # Space O(N^2)
+    def minCut(self, s: str) -> int:
+        if not s: return 0
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+
+        for length in range(n):
+            for i in range(n - length):
+                j = i + length
+
+                if i == j:
+                    dp[i][j] = True
+                elif s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] if j - i >= 2 else True
+
+        @lru_cache(None)
+        def dfs(index):
+            if index >= n:
+                return 0
+
+            res = float('inf')
+
+            for i in range(index, n):
+                if dp[index][i]:
+                    res = min(res, dfs(i + 1) + 1)
+
+            return res
+
+        return dfs(0) - 1
+
+    # Top - Down (DFS + Memo)
+    # Time O(N^3)
+    # Space O(N^2)
+    def minCut_II(self, s: str) -> int:
+        if not s: return 0
+
+        n = len(s)
+        # Double memo
+        dp = [None] * n
         dp_is_palindrome = [[None] * n for _ in range(n)]
-        
+
         def is_palindrome(i, j):
             if i >= j:
                 return True
+
             if dp_is_palindrome[i][j] != None:
                 return dp_is_palindrome[i][j]
-            
-            if s[i] == s[j] and is_palindrome(i+1, j-1):
+
+            if s[i] == s[j] and is_palindrome(i + 1, j - 1):
                 dp_is_palindrome[i][j] = True
                 return True
-            
+
             dp_is_palindrome[i][j] = False
-            
+
             return False
-        
+
         def dfs(index):
             if index >= len(s):
                 return 0
-            
+
             if dp[index] != None:
                 return dp[index]
 
@@ -34,11 +71,9 @@ class Solution:
             for i in range(index, len(s)):
                 if is_palindrome(index, i):
                     res = min(res, 1 + dfs(i + 1))
-            
+
             dp[index] = res
-            
+
             return res
-            
-        ans = dfs(0) - 1
-        
-        return 0 if ans == float('inf') else ans
+
+        return dfs(0) - 1
---
+++ leetcode/recursion + memo/1745. Palindrome Partitioning IV/py/main.py
@@ -0,0 +1,108 @@
+from functools import lru_cache
+
+
+class Solution:
+    # Precompute DP
+    # Time O(N^2)
+    # Space O(N^2)
+    def checkPartitioning(self, s):
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+
+        for length in range(n):
+            for i in range(n - length):
+                j = i + length
+
+                if i == j:
+                    dp[i][j] = True
+                elif s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] if j - i >= 2 else True
+
+        # Brute force 3 segments (with 2 cut pointers) and check if all of them are palindrome.
+        for i in range(n):
+            for j in range(i + 1, n):
+                left = dp[0][i - 1]
+                center = dp[i][j - 1]
+                right = dp[j][n - 1]
+
+                if left and center and right:
+                    return True
+
+        return False
+
+    # Precompute DP + DFS + Memo
+    # Time O(N^2)
+    # Space O(N^2)
+    def checkPartitioning_II(self, s):
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+
+        for length in range(n):
+            for i in range(n - length):
+                j = i + length
+
+                if i == j:
+                    dp[i][j] = True
+                elif s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] if j - i >= 2 else True
+
+        @lru_cache(None)
+        def dfs(index, cntPairs):
+            if index >= n:
+                return cntPairs == 0
+
+            if cntPairs < 0:
+                return False
+
+            return any(dfs(i + 1, cntPairs - 1) for i in range(index, n) if dp[index][i])
+
+        return dfs(0, 3)
+
+    # Double dp
+    # Dp + Memo
+    # Time O(N^3)
+    # Space O(N^2)
+    def checkPartitioning_II(self, s):
+        n = len(s)
+        target = 3
+        memo = {}
+        dpDpindrome = [[None] * n for _ in range(n)]
+
+        def dpindrome(left, right):
+            if left >= right:
+                return True
+
+            if dpDpindrome[left][right] != None:
+                return dpDpindrome[left][right]
+
+            if s[left] == s[right] and dpindrome(left + 1, right - 1):
+                dpDpindrome[left][right] = True
+                return True
+
+            dpDpindrome[left][right] = False
+
+            return False
+
+        def dfs(index, cntPairs):
+            if index >= n:
+                return cntPairs == target
+
+            if cntPairs > target:
+                return False
+
+            if (index, cntPairs) in memo:
+                return memo[index, cntPairs]
+
+            for i in range(index, n):
+                if dpindrome(index, i):
+                    res = dfs(i + 1, cntPairs + 1)
+
+                    if res:
+                        memo[index, cntPairs] = True
+                        return True
+
+            memo[index, cntPairs] = False
+
+            return False
+
+        return dfs(0, 0)
